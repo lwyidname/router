8f7ef2d27d5a0afc6bbb21ab698c05dbcf8a82b4
liwenyu <lwyidname@163.com>
2020-04-10 17:50:12 +0800

    add

---
+++ src/views/database/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-container">123</div>
+</template>
+
+<script>
+export default {
+  name: "Database",
+  data() {
+    return {
+      databasePid: ""
+    };
+  },
+  watch: {},
+  created() {
+    if (location.hash.split("/")[2] === "poorSealingDatabase") {
+      // 封孔不良
+      this.databasePid = "2";
+    } else if (location.hash.split("/")[2] === "goafDatabase") {
+      // 矿井及周边采空区
+      this.databasePid = "3";
+    }
+    this.getPage(this.databasePid, "");
+  },
+
+  methods: {
+    getPage(id, name) {
+      console.log(id);
+    }
+  }
+};
+</script>
+
